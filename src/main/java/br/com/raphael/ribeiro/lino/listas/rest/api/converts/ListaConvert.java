6b41aacbfe2af23aa2d56abfd99b3c100b44079e
Raphael Ribeiro Lino <raphael.lino@gpnet.com.br>
2022-09-17 12:01:26 -0300

    Adicionando converts

---
+++ src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/converts/ListaConvert.java
@@ -0,0 +1,12 @@
+package br.com.raphael.ribeiro.lino.listas.rest.api.converts;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListaConvert {
+
+	@Autowired
+	private ModelMapper modelMapper;
+}
