8ea25c106fe4129544baffd1f5ba585aca6acd24
Raphael Ribeiro Lino <raphael.lino@gpnet.com.br>
2022-09-17 13:16:37 -0300

    Adicionando endpoint que retorna uma lista pelo id da lista

--- src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/controllers/ListaController.java
+++ src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/controllers/ListaController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,4 +56,12 @@ public class ListaController {
 		List<ListaEntity> listas = listaService.listaTodas();
 		return listaConvert.listaEntityToListOutput(listas);
 	}
+	
+	@GetMapping("/{id}")
+	public ListaOutput buscaListaPorId(@PathVariable Long id) {
+		ListaEntity listaEncontrada = listaService.buscaPorId(id);
+		return listaConvert.entityToOutput(listaEncontrada);
+	}
+	
+
 }
--- src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/services/ListaService.java
+++ src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/services/ListaService.java
@@ -30,4 +30,6 @@ public class ListaService {
 	public List<ListaEntity> listaTodas() {
 		return listaRepository.findAll();
 	}
+
+	
 }
