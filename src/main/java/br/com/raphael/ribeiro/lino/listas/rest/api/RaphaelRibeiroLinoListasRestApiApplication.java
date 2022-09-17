dce28e87490e72e0c62cfeca2a6cd9fc528d9304
Raphael Ribeiro Lino <raphael.lino@gpnet.com.br>
2022-09-17 10:32:40 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/raphael/ribeiro/lino/listas/rest/api/RaphaelRibeiroLinoListasRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphael.ribeiro.lino.listas.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaphaelRibeiroLinoListasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaphaelRibeiroLinoListasRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raphael/ribeiro/lino/listas/rest/api/RaphaelRibeiroLinoListasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphael.ribeiro.lino.listas.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaphaelRibeiroLinoListasRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
